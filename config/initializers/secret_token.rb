1f07435ebab750f4f9615ba663d66f16a9d80a08
Bill Curtis <wcurtis@sweettoothhq.com>
2014-01-08 22:23:22 -0500

    Initial commit with bare rails app from ember.js template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PongPoints::Application.config.secret_key_base = '8ebdd26781bfa82539fc0180a199ddd316f06e7edac09fa518ef72e143575978f1c0c65692392adae299574fd387777d60293dbb2215979bdbae74d7b0391f6c'
