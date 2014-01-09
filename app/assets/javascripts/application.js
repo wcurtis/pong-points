07662539b882064deee71a81722909781b025076
Bill Curtis <wcurtis@sweettoothhq.com>
2014-01-08 22:26:38 -0500

    Ran ember:bootstrap to generate ember dir structure

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require pong_points
+
+// for more details see: http://emberjs.com/guides/application/
+PongPoints = Ember.Application.create();
+
 //= require_tree .
