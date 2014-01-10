1d6ef96032be791aebf6e93ba72ca832c22f7e56
Bill Curtis <wcurtis@sweettoothhq.com>
2014-01-10 18:50:56 -0500

    Now creating two seed games

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,17 @@ players = Player.create([
   { first_name: 'Wayne', last_name: "Rooney", profile_image_url: "/assets/players/rooney.jpg" },
   { first_name: 'Leonel', last_name: "Messi", profile_image_url: "/assets/players/messi.jpg" }
 ])
+
+# Crate a finished game
+game = Game.new({ status: 'finished' })
+game.players.push(players[0])
+game.players.push(players[1])
+game.winner = players[0].id
+game.save
+
+# Create an in progress game
+game = Game.new({ status: 'in_progress' })
+game.players.push(players[0])
+game.players.push(players[1])
+game.save
+
