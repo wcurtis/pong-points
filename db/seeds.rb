5e3a35563bbac60fd0e1c289edd75b715c214a09
Bill Curtis <wcurtis@sweettoothhq.com>
2014-01-10 14:31:23 -0500

    Added player seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+players = Player.create([
+  { id: 1, first_name: 'Wayne', last_name: "Rooney", profile_image_url: "/assets/players/rooney.jpg" },
+  { id: 2, first_name: 'Leonel', last_name: "Messi", profile_image_url: "/assets/players/messi.jpg" }
+])
