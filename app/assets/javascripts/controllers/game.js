223cd9ead2b9160a9965e846ec6827a0d9534f89
Bill Curtis <wcurtis@sweettoothhq.com>
2014-01-10 00:33:32 -0500

    Can now navigat to homepage from game confirmation

--- app/assets/javascripts/controllers/game.js
+++ app/assets/javascripts/controllers/game.js
@@ -1,5 +1,9 @@
 App.GameController = Ember.ObjectController.extend({
 
+  gameInProgress: function() {
+    return this.get('status') === 'in_progress';
+  }.property('status'),
+
   actions: {
     finish: function() {
       this.set('winner', this.get('player1'));
