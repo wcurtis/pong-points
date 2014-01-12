d11964997d64c51e7e9d07e90902b4cb58cea7c4
Bill Curtis <wcurtis@sweettoothhq.com>
2014-01-12 16:14:47 -0500

    Added httparty gem to make calls to ST API, will switch to the ST Rails lib once it's built

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,27 @@
+require "httparty"
+
 class Player < ActiveRecord::Base
   has_and_belongs_to_many :games
+
+  after_initialize do |user|
+
+    credentials = "sk_hwHybrSsMLvUj7Crt8Cz142Y:"
+
+    headers = {
+      'Accept' => "application/json",
+      'Content-Type' => "application/json",
+      'Authorization' => "Basic #{Base64.encode64(credentials)}"
+    }
+
+    stUserData = {
+      "email" => user.email
+    }
+
+    response = HTTParty.get('https://api.sweettooth.io/v1/customers', { :headers => headers })
+    stUser = JSON.parse(response.body)
+    puts "You have initialized " + user.email;
+    puts stUser.inspect
+  end
 
   def name
     return first_name + ' ' + last_name
