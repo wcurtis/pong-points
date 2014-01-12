8fada25c4c8b927492791b2f72ea7e473eef6216
Bill Curtis <wcurtis@sweettoothhq.com>
2014-01-11 21:41:29 -0500

    Can now toggle players, two max, controllers are epic

--- app/assets/javascripts/controllers/games/new.js
+++ app/assets/javascripts/controllers/games/new.js
@@ -1,5 +1,9 @@
 App.GamesNewController = Ember.ArrayController.extend({
 
+  itemController: 'gamesItem',
+
+  selected: [],
+
   actions: {
     save: function() {
 
@@ -15,6 +19,30 @@ App.GamesNewController = Ember.ArrayController.extend({
       game.save().then(function() {
         self.transitionToRoute('game', game);
       });
+    },
+
+    /**
+     * Learned a ton from this blog on how to use controllers
+     * to do item selection the Ember way, this article is gold :)
+     * http://gaslight.co/blog/intermediate-ember-controller-concepts
+     */
+    toggleSelected: function(controller) {
+
+      var id = controller.get('id');
+      var selected = this.get('selected');
+
+      // Deselect if currently selected
+      if (selected.contains(id)) {
+        controller.deselect();
+        this.set('selected', selected.without(id));
+        return;
+      }
+
+      // Limit selection to 2 players
+      if (selected.length < 2) {
+        selected.push(id);
+        controller.select();
+      }
     }
   }
 
