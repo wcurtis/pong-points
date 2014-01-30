b353a97dabc7bf682db4882c704f8617f9520f28
Bill Curtis <wcurtis@sweettoothhq.com>
2014-01-29 19:43:27 -0500

    Now filtering games on home page to only show finished ones

--- app/assets/javascripts/controllers/index.js
+++ app/assets/javascripts/controllers/index.js
@@ -1,6 +1,12 @@
 App.IndexController = Ember.ArrayController.extend({
 
   sortProperties: ['created_at'],
-  sortAscending: false
+  sortAscending: false,
+
+  // Filters games by finished status so we don't show
+  // any in_progress or cancelled games on the dashboard
+  completedGames: (function() {
+    return this.get('content').filterProperty('status', 'finished');
+  }).property('content.@each.status', 'content')
 
 });
