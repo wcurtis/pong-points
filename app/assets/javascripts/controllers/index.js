0cf7097387b66899ddbdeb6c4dddd92a1c11f81f
Bill Curtis <wcurtis@sweettoothhq.com>
2014-01-29 20:02:23 -0500

    Fixed sorting of games

--- app/assets/javascripts/controllers/index.js
+++ app/assets/javascripts/controllers/index.js
@@ -5,8 +5,15 @@ App.IndexController = Ember.ArrayController.extend({
 
   // Filters games by finished status so we don't show
   // any in_progress or cancelled games on the dashboard
+  // More about ArrayProxies: http://stackoverflow.com/a/13397509/540194
   completedGames: (function() {
-    return this.get('content').filterProperty('status', 'finished');
-  }).property('content.@each.status', 'content')
+
+    var content = this.get("content") || [];
+    return Ember.ArrayProxy.createWithMixins(Ember.SortableMixin, {
+      content: content.toArray(),
+      sortProperties: this.get('sortProperties'),
+      sortAscending: this.get('sortAscending')
+    });
+  }).property('content.@each.status', 'content', 'sortProperties', 'sortAscending')
 
 });
